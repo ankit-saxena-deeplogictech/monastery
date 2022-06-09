c951adb2fb70bb421c5106104c8ab210ffe1b535
Ankit Saxena <ankit.saxena@deeplogictech.com>
2022-06-09 12:35:35 +0530

    sanity of open.mjs

--- frontend/apps/monastery/views/api400-designer/components/pluggable-ribbon/topribbon/open/open.mjs
+++ frontend/apps/monastery/views/api400-designer/components/pluggable-ribbon/topribbon/open/open.mjs
@@ -1,63 +1,62 @@
 /* 
  * (C) 2020 TekMonks. All rights reserved.
  */
-import { view } from "../../../../view.mjs"
-import { i18n } from "/framework/js/i18n.mjs";
-import { util } from "/framework/js/util.mjs";
-import { blackboard } from "/framework/js/blackboard.mjs";
-import { serverManager } from "../../../../js/serverManager.js";
-import { page_generator } from "/framework/components/page-generator/page-generator.mjs";
+import {view} from "../../../../view.mjs"
+import {i18n} from "/framework/js/i18n.mjs";
+import {util} from "/framework/js/util.mjs";
+import {blackboard} from "/framework/js/blackboard.mjs";
+import {serverManager} from "../../../../js/serverManager.js";
+import {page_generator} from "/framework/components/page-generator/page-generator.mjs";
 import {apiclparser} from "../../../../model/apiclparser.mjs"
 
-
-const PLUGIN_PATH = util.getModulePath(import.meta), MSG_FILE_UPLOADED = "FILE_UPLOADED",
+const PLUGIN_PATH = util.getModulePath(import.meta), MSG_FILE_UPLOADED = "FILE_UPLOADED", 
     CONTEXT_MENU = window.monkshu_env.components["context-menu"], CONTEXT_MENU_ID = "contextmenumain",
-    DIALOG_RET_PROPS = ["name", "server", "port", "adminid", "adminpassword"],
-    DIALOG = window.monkshu_env.components["dialog-box"], VIEW_PATH = `${PLUGIN_PATH}/../../../../`;
+    DIALOG_RET_PROPS = ["name", "server", "port", "adminid", "adminpassword"], 
+    DIALOG = window.monkshu_env.components["dialog-box"], VIEW_PATH=`${PLUGIN_PATH}/../../../../`;
 let IMAGE, I18N, saved_props;
 
 async function init() {
     const svgSource64 = btoa(await (await fetch(`${PLUGIN_PATH}/open.svg`)).text());
     IMAGE = "data:image/svg+xml;base64," + svgSource64;
-    I18N = (await import(`${PLUGIN_PATH}/open.i18n.mjs`)).i18n;
+    I18N = (await import(`${PLUGIN_PATH}/open.i18n.mjs`)).i18n; 
     return true;
 }
 
 async function clicked(element, event) {
     event.stopPropagation(); const lang = i18n.getSessionLang();
-    const menus = {}; menus[`${I18N.NEW[lang]}`] = _ => view.reset(); menus[`${I18N.OPEN_FROM_DISK[lang]}`] = _ => _uploadFile();
-    menus[`${I18N.OPEN_FROM_SERVER[lang]}`] = _ => _getFromServer();
+    const menus = {}; menus[`${I18N.NEW[lang]}`] = _=>view.reset(); menus[`${I18N.OPEN_FROM_DISK[lang]}`] = _=>_uploadFile();
+    menus[`${I18N.OPEN_FROM_SERVER[lang]}`] = _=>_getFromServer();
     const boundingRect = element.getBoundingClientRect(), x = boundingRect.x, y = boundingRect.bottom;
     CONTEXT_MENU.showMenu(CONTEXT_MENU_ID, menus, x, y, 0, 5);
 }
 
 const getImage = _ => IMAGE;
 
-const getHelpText = (lang = en) => I18N.HELP_TEXTS[lang];
+const getHelpText = (lang=en) => I18N.HELP_TEXTS[lang];
 
-const getDescriptiveName = (lang = en) => I18N.DESCRIPTIVE_NAME[lang];
+const getDescriptiveName = (lang=en) => I18N.DESCRIPTIVE_NAME[lang];
 
 const allowDrop = event => _isDraggedItemAJSONFile(event);
 
 async function droppedFile(event) {
     event.preventDefault(); if (!_isDraggedItemAJSONFile(event)) return; // can't support whatever was dropped
-    const file = event.dataTransfer.items[0].getAsFile();
+    const file = event.dataTransfer.items[0].getAsFile(); 
     try {
-        const { name, data } = await util.getFileData(file);
-        blackboard.broadcastMessage(MSG_FILE_UPLOADED, { name, data });
-    } catch (err) { LOG.error(`Error opening file: ${err}`); }
+        const {name, data} = await util.getFileData(file);
+        blackboard.broadcastMessage(MSG_FILE_UPLOADED, {name, data});
+    } catch (err) {LOG.error(`Error opening file: ${err}`);}
 }
 
 async function _uploadFile() {
     try {
-        let { name, data } = await util.uploadAFile("application/json");
+        let {name, data} = await util.uploadAFile("application/json");
         data = JSON.stringify(await apiclparser.apiclParser(data));
-        blackboard.broadcastMessage(MSG_FILE_UPLOADED, { name, data });
-    } catch (err) { LOG.error(`Error opening file: ${err}`); }
+        blackboard.broadcastMessage(MSG_FILE_UPLOADED, {name, data});
+    } catch (err) {LOG.error(`Error opening file: ${err}`);}
 }
-
+    
 async function _getFromServer() {
-    const pageFile = util.resolveURL(`${VIEW_PATH}/dialogs/dialog_openserver.page`);
+    const pageFile =  util.resolveURL(`${VIEW_PATH}/dialogs/dialog_openserver.page`);
 
     let html = await page_generator.getHTML(new URL(pageFile));
 
@@ -82,8 +81,7 @@ async function _getFromServer() {
         });
 }
 
-
 const _isDraggedItemAJSONFile = event => event.dataTransfer.items?.length && event.dataTransfer.items[0].kind === "file"
     && event.dataTransfer.items[0].type.toLowerCase() === "application/json";
 
-export const open = { init, clicked, getImage, getHelpText, getDescriptiveName, allowDrop, droppedFile }
+export const open = {init, clicked, getImage, getHelpText, getDescriptiveName, allowDrop, droppedFile}
