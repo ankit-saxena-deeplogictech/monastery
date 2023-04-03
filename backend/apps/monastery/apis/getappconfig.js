daa0028eb4462c2849a9d9135f8032519d63af9f
Sasidhar-dlt <sasidhar.katikela@deeplogictech.com>
2023-04-03 13:03:21 +0530

    Package name@openfrmserver@publish,caseinsensitive

--- backend/apps/monastery/apis/getappconfig.js
+++ backend/apps/monastery/apis/getappconfig.js
@@ -3,6 +3,6 @@ exports.doService = async jsonReq => {
 	
 	LOG.info("returning server details");
 
-	return JSON.stringify({ "host" : "127.0.0.1" ,"port" : 7080,"adminid" : "admin","adminpassword" : "DLT4TekMonks","secure":false,"name":"ankit"});
+	return JSON.stringify({ "host" : "127.0.0.1" ,"port" : 7080,"adminid" : "admin","adminpassword" : "DLT4TekMonks","secure":false,"name":"bhavesh"});
 }
 
