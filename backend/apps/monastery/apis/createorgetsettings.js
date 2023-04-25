9fc2497636f336e6d8acfae73483c018216cb8bd
Sasidhar-dlt <sasidhar.katikela@deeplogictech.com>
2023-04-25 13:13:48 +0530

    show apis based on settings - initail commit

--- backend/apps/monastery/apis/createorgetsettings.js
+++ backend/apps/monastery/apis/createorgetsettings.js
@@ -12,9 +12,9 @@ exports.doService = async jsonReq => {
     const jsonData = fs.readFileSync(`${APP_CONSTANTS.CONF_DIR}/settings.json`, 'utf8');
     const data = JSON.parse(jsonData);
     if (!data.hasOwnProperty(jsonReq.org)) {
-        data[jsonReq.org] = {server:"",port:"",apikey:""};
+        data[jsonReq.org] = {server:"",port:"",apikey:"",publicapikey:""};
         fs.writeFileSync(`${APP_CONSTANTS.CONF_DIR}/settings.json`,JSON.stringify(data, null, 4))
-        return { result:result,data:{server:"",port:"",apikey:""}};
+        return { result:result,data:{server:"",port:"",apikey:"",publicapikey:""}};
     }
     else  return { result:result,data:data[jsonReq.org]};
     }
--- backend/apps/monastery/apis/setdefaultsettings.js
+++ backend/apps/monastery/apis/setdefaultsettings.js
@@ -11,7 +11,11 @@ exports.doService = async jsonReq => {
     const jsonData = fs.readFileSync(`${APP_CONSTANTS.CONF_DIR}/settings.json`, 'utf8');
     const data = JSON.parse(jsonData);
     if (data.hasOwnProperty(jsonReq.org) ) {
-        data[jsonReq.org] = {server:jsonReq.server,port:jsonReq.port,apikey:jsonReq.apikey};
+        const apiKey = data[jsonReq.org]["apikey"];
+        const publicApikey = data[jsonReq.org]["publicapikey"];
+
+        data[jsonReq.org] = {server:jsonReq.server,port:jsonReq.port,apikey:jsonReq.apikey,publicapikey:publicApikey };
+        if(jsonReq.isPublic) data[jsonReq.org] = {server:jsonReq.server,port:jsonReq.port,publicapikey:jsonReq.apikey,apikey:apiKey};
         fs.writeFileSync(`${APP_CONSTANTS.CONF_DIR}/settings.json`,JSON.stringify(data, null, 4))
         return { result:result };
     }
